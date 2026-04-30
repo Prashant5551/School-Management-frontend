10c5092b16c65ac1fcae26cf4429c8e2297e167f
Prashant5551 <prashkbk@gmail.com>
2026-04-30 19:00:00 +0530

    Events code added

--- src/app/features/events/events.ts
+++ src/app/features/events/events.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Api } from '../../services/api';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-events',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './events.html',
   styleUrl: './events.scss'
 })
@@ -10,6 +12,36 @@ export class Events {
 
   ngOnInit() {
     window.scrollTo(0, 0);
+    this.getEvents();
   }
+
+  events: any = [];
+  selectedEvent: any = {
+    title: '',
+    date: '',
+    location: '',
+    description: '',
+    shortDescription: ''
+  }
+  constructor(private apiService: Api) { }
+
+  getEvents() {
+    this.apiService.getEvents().subscribe({
+      next: (response: any) => {
+        if (response && response['status'] === 'Y') {
+          this.events = response.data;
+          console.log(this.events, '.......');
+        }
+      },
+      error(error: any) {
+        console.log(error);
+      }
+    })
+  }
+
+  showEvent(event: any) {
+    this.selectedEvent = event;
+  }
+
 
 }
